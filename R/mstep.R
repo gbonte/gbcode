2b4aa5ab11a309b6a5a5503a3595f70b34f9500e
gbonte <gbonte@ulb.ac.be>
2018-01-12 08:55:59 +0100

    ms

--- R/mstep.R
+++ R/mstep.R
@@ -693,6 +693,8 @@ multiplestepAhead<-function(TS,n,H,D=0, method="direct",Kmin=3,C=2,FF=0,smooth=F
            CPar=c(Kmin,C*Kmin)
            CPar[1]=min(CPar[1],NROW(X)-1)
            CPar[2]=min(CPar[2],NROW(X))
+           print(dim(X))
+           print(CPar)
           
            if (NROW(X) <= 5*NCOL(X))
              LPar=NULL
