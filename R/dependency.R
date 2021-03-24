4f7d568155cf8e719294636fd122ab7116c8af72
Gianluca Bontempi <gianlucabontempi@MacBook-Pro-de-Gianluca.local>
2021-03-24 19:40:08 +0100

    dep

--- R/dependency.R
+++ R/dependency.R
@@ -1,5 +1,58 @@
 #' @import mvtnorm MASS
 
+
+
+
+Posdef <- function (n, ev = runif(n, 0, 10)) {
+  Z <- matrix(ncol=n, rnorm(n^2))
+  decomp <- qr(Z)
+  Q <- qr.Q(decomp)
+  R <- qr.R(decomp)
+  d <- diag(R)
+  ph <- d / abs(d)
+  O <- Q %*% diag(ph)
+  Z <- t(O) %*% diag(ev) %*% O
+  return(Z)
+}
+
+nonlinfct<-function(X,f=1){
+  n<-NCOL(X)
+  N<-NROW(X)
+  
+  fx<-sample(n,sample(2:(n-2),1))
+  fy<-setdiff(1:n,fx)
+  #cat(fx,":",fy,";",n,"\n")
+  L=length(fx)
+  if (L==1){
+    x=abs(X[,fx])
+  } else {
+    x=apply(abs(X[,fx]),1,mean)
+  }
+  if (length(fy)==1){
+    y=abs(X[,fy])
+  } else {
+    y=apply(abs(X[,fy]),1,mean)
+  }
+  switch(f,
+         {Yhat=log(x^2+y^2)},
+         {Yhat=sqrt(abs(sin(x^2+y^2)))},
+         {Yhat=log(x*y^2+x^2*y)},
+         {Yhat=sqrt(abs(x^2/(y+1)))},
+         {Yhat=1/(x^2+y^2+1)},
+         {Yhat=(x*sin(x*y))/(x^2+y^2+1)},
+         {Yhat=y*exp(2*x^2)},
+         {Yhat=y*sin(x)+x*sin(y)},
+         {Yhat=(x^3-2*x*y+y^2)/(x^2+y^2+1)},
+         {Yhat=x+y+log(abs(x*y))},
+         {Yhat=sin(x)+log(y)},
+         {Yhat=sin(pi*x*y)})
+  
+  return(Yhat)
+}
+
+
+
+
 #### regrDataset ####
 #' Generator of nonlinear regression dataset
 #'
@@ -32,54 +85,9 @@ regrDataset<-function(N,n,neff,sdn,seed=0){
   Sigma=Posdef(n)
   X<-scale(rmvnorm(N,sigma=Sigma ))
   feat<-sample(n,neff)
-  Y<-nonlin.fct(X[,feat],sample(11,1))+rnorm(N,sd=sdn)
+  XX=X[,feat]
+  Y<-nonlinfct(XX,sample(11,1))+rnorm(N,sd=sdn)
   list(X=X,Y=Y,feat=feat)
 }
 
-
-
-
-Posdef <- function (n, ev = runif(n, 0, 10)) {
-Z <- matrix(ncol=n, rnorm(n^2))
-decomp <- qr(Z)
-Q <- qr.Q(decomp)
-R <- qr.R(decomp)
-d <- diag(R)
-ph <- d / abs(d)
-O <- Q %*% diag(ph)
-Z <- t(O) %*% diag(ev) %*% O
-return(Z)
-}
-
-nonlin.fct<-function(X,f=1){
-  n<-NCOL(X)
-  N<-NROW(X)
-
-  fx<-sample(n,sample(2:(n-2),1))
-  fy<-setdiff(1:n,fx)
-  if (length(fx)==1)
-    x=abs(X[,fx])
-  else
-    x=apply(abs(X[,fx]),1,mean)
-
-  if (length(fy)==1)
-    y=abs(X[,fy])
-  else
-    y=apply(abs(X[,fy]),1,mean)
-
-  switch(f,
-         {Yhat=log(x^2+y^2)},
-         {Yhat=sqrt(abs(sin(x^2+y^2)))},
-         {Yhat=log(x*y^2+x^2*y)},
-         {Yhat=sqrt(abs(x^2/(y+1)))},
-         {Yhat=1/(x^2+y^2+1)},
-         {Yhat=(x*sin(x*y))/(x^2+y^2+1)},
-         {Yhat=y*exp(2*x^2)},
-         {Yhat=y*sin(x)+x*sin(y)},
-         {Yhat=(x^3-2*x*y+y^2)/(x^2+y^2+1)},
-         {Yhat=x+y+apply(X[,fx],1,mean)},
-         {Yhat=sin(x)+log(y)})
-
-  return(Yhat)
-}
 
