499f1d5138e56ff2a3e2e360370fdf1762af8b86
Gianluca Bontempi <gbonte@ulb.ac.be>
2022-11-16 16:57:54 +0100

    ridge

--- R/mstep.R
+++ R/mstep.R
@@ -847,7 +847,7 @@ multiplestepAhead<-function(TS,n,H,D=0, method="direct",
   }
   
   if (method=="mimolin"){
-    p=multiridge(array(TS,c(length(TS),1)),n,H,B=0,QRdec=FALSE,verbose=verbose,...) 
+    p=multiridge(array(TS,c(length(TS),1)),n,H,B=0,QRdec=FALSE,verbose=verbose,...)$Yhat 
     return(c(p+trnd.ts))
   }
   
