900b69b93a952d0986cddca00364e7ac26047ed8
Gianluca Bontempi <gianlucabontempi@MacBook-Pro-de-Gianluca.local>
2023-01-26 09:25:06 +0100

    demo_fore

--- inst/scripts/Forecasting/demo_fore.R
+++ inst/scripts/Forecasting/demo_fore.R
@@ -2,7 +2,7 @@ rm(list=ls())
 library(reticulate)
 library(gbcode)
 
-library(keras)
+
 set.seed(0)
 N=200
 m=3
@@ -20,12 +20,12 @@ N=NROW(TS)
 if (m>1){
   #P=MmultiplestepAhead(TS[1:(N-H),],n=n,H=H,multi="RNN",
   #                    nepochs=100, nunits=10)
-  #P=MmultiplestepAhead(TS[1:(N-H),],n=n,H=H,multi="MIMO_rr",
-  #                     nLambdas=50)
-  P=MmultiplestepAhead(TS[1:(N-H),],n=n,H=H,multi="TRANSF",epochs=1000)
+  P=MmultiplestepAhead(TS[1:(N-H),],n=n,H=H,multi="MIMO_rr",
+                       nLambdas=50)
+  ##P=MmultiplestepAhead(TS[1:(N-H),],n=n,H=H,multi="TRANSF",epochs=1000)
 } else {
-  ## P=multiplestepAhead(TS[1:(N-H),],n=n,H=H,method="mimo_rr",epochs=1000)
-  P=multiplestepAhead(TS[1:(N-H),],n=n,H=H,method="transf",epochs=1000)
+  P=multiplestepAhead(TS[1:(N-H),],n=n,H=H,method="mimo_rr",epochs=1000)
+  ##P=multiplestepAhead(TS[1:(N-H),],n=n,H=H,method="transf",epochs=1000)
   
 }
 
